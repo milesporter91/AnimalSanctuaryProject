4e170ab2f8286e396c12d715e16a37f98ac4789d
Miles Porter <portermiles91@gmail.com>
2025-03-14 16:05:32 -0600

    initial commit

---
+++ src/com/skilldistillery/animals/app/AnimalSanctuaryApp.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.animals.app;
+
+import java.util.Scanner;
+
+public class AnimalSanctuaryApp {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		
+	}
+
+}
