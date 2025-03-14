7b693a43b0ccadf0200ed1567cc47af31f27777e
Miles Porter <portermiles91@gmail.com>
2025-03-14 17:44:06 -0600

    adds Santuary class and completes outline for Attendant class

--- src/com/skilldistillery/animals/entities/Attendant.java
+++ src/com/skilldistillery/animals/entities/Attendant.java
@@ -6,8 +6,12 @@ public class Attendant {
 			if (! animals[i].equals(null)) {
 				System.out.println(animals[i].getName() + " is a " + animals[i].getClass() 
 						+ " who LOVES bamboo stalks!");
-				
+				animals[i].makeNoise();
+				animals[i].eat((int)Math.random() * 10 + 1);
+			} else {
+				System.out.println("This enclosure is empty for now! Let us know if you find any primates out there!");
 			}
 		}
+		System.out.println("Woo! All done for now! Time to take a break!");
 	}
 }
